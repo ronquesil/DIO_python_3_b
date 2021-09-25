758475e07a8b3a432c0221472bc8017d59f65a37
Ronaldo Queiroz <ronquesil@gmail.com>
2021-09-24 22:25:38 -0300

    Exercícios

---
+++ aula2.py
@@ -0,0 +1,18 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('Soma: {}. \nSubtração: {}. \nMultiplicação: {}. \nDivisão: {}. \nResto: {}.'.format(soma, subtracao, multiplicacao, divisao, resto))
+# print('soma: ' + str(soma))
+# print(soma)
+# print(subtracao)
+# print(multiplicacao)
+# print(int(divisao))
+# print(divisao)
+# print(resto)
+# x = '1'
+# soma2 = int(x) + 1
+# print(soma2)
